55ba58078a8a9b0361c21a9e9fc0f0fc1fc9febe
saikiran sirineni <saikiran.sirineni@gmail.com>
2023-11-12 09:49:03 +0530

    commit

---
+++ Telsuko_Java_Course/#21 Class And Object Theory in Java/Sample.java
@@ -0,0 +1,14 @@
+public class Sample
+{
+    public static void main(String[] args) 
+    {
+
+    }
+}
+
+// Object Oriented Programming.
+// Object - Properties and Behaviours.
+
+// To create an object we need a class.
+// Class is a blueprint
+// JVM creates objects in Java using blueprint.
